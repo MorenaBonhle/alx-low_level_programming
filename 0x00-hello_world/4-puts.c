7e3638a3be4576715a4921efbf675afdfe4453cc
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-18 15:43:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#!/bin/bash
+
 #include <stdio.h>
+
+/**
+ * main - The function prints out a text
+ *
+ * Return: (0)
+ */
 
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle"");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
