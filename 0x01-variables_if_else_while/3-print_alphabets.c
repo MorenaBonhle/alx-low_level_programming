4a0c2f6b94c2e2d8f354aa3f2c1d5f8b6c6a72bd
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-20 05:44:35 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints the lowercase character
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for(ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
