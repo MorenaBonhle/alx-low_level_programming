cf0a6ab2305162837fbc50ff54dcc582b382896e
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-20 13:34:38 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints the lowercase character
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+if (i == 58)
+{
+for (i = 97; i <=102; i++)
+{
+putchar(i);
+}
+}
+putchar(i);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
