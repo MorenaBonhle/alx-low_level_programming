d2b1e2d13111b1a7b54188be88d9189df5027790
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-20 06:18:34 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints the lowercase character
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
