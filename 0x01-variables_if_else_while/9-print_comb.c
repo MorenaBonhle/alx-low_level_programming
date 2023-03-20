18342002eb9032544860a2cc072f4f88c12a3534
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-20 14:06:51 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints the numbers
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 48; i < 57; i++)
+{
+putchar(i);
+putchar(44);
+putchar(32);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+}
