70aeab75848533cac572a38f1abe9e70c62ca7b7
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-21 10:50:04 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,19 @@
 void print_alphabet_x10(void)
 {
 char ch;
-int i = 0;
+int i;
+
+i = 0;
 
 while (i < 10)
 {
 ch = 'a';
 while(ch <= 'z')
 {
+ _putchar(ch);
+ ch++;
+}
  _putchar('\n');
  i++;
-}
 }
 }
