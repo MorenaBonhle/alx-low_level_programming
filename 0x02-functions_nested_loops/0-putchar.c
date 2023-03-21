d62fd92cacb56d52ff78f185d4573d962fa16817
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-21 07:24:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints the words
+ *
+ * Return: (0)
+ */
+
+
+int main( void ){
+char words[] = "_putchar";
+int i;
+    
+for (int i = 0; i < 9; i++)
+{
+if (words[i] == '\0')
+{
+putchar('\n');
+break;
+}
+else
+putchar(words[i]);
+}
+
+/* your code goes there */
+return (0);
+}
