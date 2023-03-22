689b7bc8c4e2b5f1d3111c821217fb8b32e29ead
MorenaBonhle <ultimateperfection22@gmail.com>
2023-03-22 13:11:29 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,16 @@
  * Prints ten times the alphabets
  */
 
+/* The funcion that prints alphabets*/
+
 void print_alphabet_x10(void)
 {
 char ch;
 int i;
 
 i = 0;
+
+/* loop functions */
 
 while (i < 10)
 {
